6ce0eaf85fc387114c80204f73bbc3df2f4f739f
mamba2410 <callummcgregor54@gmail.com>
2024-05-27 17:20:13 +0100

    buttons now actually call picowalker-core callback

--- src/buttons_u545_exti.c
+++ src/buttons_u545_exti.c
@@ -13,15 +13,19 @@ static uint16_t last_press = 0;
 void HAL_GPIO_EXTI_Falling_Callback(uint16_t pin) {
     if( pin == BUT1_PIN || pin == BUT2_PIN || pin == BUT3_PIN ) {
 
+        // Software debounce
         if((HAL_GetTick() - last_press) < DEBOUNCE_TIME_MS)
             return;
-
         last_press = HAL_GetTick();
-        char c = (pin == BUT1_PIN ? 'L' : (pin == BUT2_PIN ? 'M' : 'R'));
-        pw_log(3, "Button %c pressed\r\n", c);
+
+        uint8_t b = 0;
+        if(pin == BUT1_PIN) b |= BUTTON_L;
+        if(pin == BUT2_PIN) b |= BUTTON_M;
+        if(pin == BUT3_PIN) b |= BUTTON_R;
+        pw_button_callback(b);
 
     } else {
-        pw_log(3, "EXTI handled, not from buttons\r\n");
+        pw_log(2, "EXTI handled, not from buttons\r\n");
     }
 }
 
